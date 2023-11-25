307c9a2b00e89460d2e4d70f67f403483d9b7704
Justin Moore <justinamo@gmail.com>
2023-11-25 17:49:17 -0500

    qrm constructed

--- eventParser.py
+++ eventParser.py
@@ -1,4 +1,5 @@
 import blpapi
+from tick import Tick_types
 from dates import convert_from_utc
 
 tickData = blpapi.Name("tickData")
@@ -11,7 +12,43 @@ def extract_tick_data(session):
                 td = message.asElement().getElement(tickData)
                 return td.getElement(tickData)
 
-def construct_qrm(tick_data):
-    return
+def construct_qrm(ticks):
+
+    qr = []
+    recent_bbo = {"BID": None, "ASK": None}
+    last = None
+
+    for tick in ticks:
+        if tick.type == Tick_types.trade:
+            qr.append({"TRADE": tick})
+        elif tick.type == Tick_types.bid:
+            recent_bbo["BID"] = tick
+            qr.append(recent_bbo.copy())
+        elif tick.type == Tick_types.ask:
+            recent_bbo["ASK"] = tick
+            qr.append(recent_bbo.copy())
+            
+    ticks.reverse()
+    qr.reverse()
+
+    last_deleted = None
+
+    to_delete = []
+    for i in range(len(ticks)):
+        last_deleted_was_set = False
+        if ticks[i].type != Tick_types.trade and ticks[i-1].type != Tick_types.trade:
+            if ticks[i-1].type != ticks[i].type and not last_deleted:
+                to_delete.append(i)
+                last_deleted = True
+                last_deleted_was_set = True
+            if not last_deleted_was_set:
+                last_deleted = False
+
+    to_delete.reverse()
+
+    for i in to_delete:
+        del qr[i]
+
+    return qr
 
             
--- query.py
+++ query.py
@@ -4,7 +4,7 @@ Requires a connection to the Bloomberg API.
 
 import blpapi
 from request import IntradayTickRequest
-from eventParser import extract_tick_data
+from eventParser import extract_tick_data, construct_qrm
 from tick import Tick, Tick_types
 
 
@@ -26,45 +26,7 @@ request.include_detail()
 request.send(session)
 tick_data = extract_tick_data(session)
 ticks = list(map(Tick.of_tickdata_singleton, tick_data))
+qrm = construct_qrm(ticks)
+    
 
-qr = []
-recent_bbo = {"BID": None, "ASK": None}
-last = None
-before_last = None
-completed_pair = False
-
-for tick in ticks: 
-    popped_last_was_set = False
-    tmp = last
-    print(tick)
-    if tick.type == Tick_types.trade:
-        qr.append({"trade": tick})
-        last = "TRADE"
-    elif tick.type == Tick_types.bid:
-        recent_bbo["BID"] = tick
-        if recent_bbo["ASK"] is not None:
-            if last == "ASK" and not popped_last and not before_last == "TRADE":
-                qr.pop()
-                popped_last = True
-                popped_last_was_set = True
-            qr.append(recent_bbo.copy())
-            if not popped_last_was_set:
-                popped_last = False
-        last = "BID"
-    elif tick.type == Tick_types.ask:
-        recent_bbo["ASK"] = tick
-        if recent_bbo["BID"] is not None:
-            if last == "BID" and not popped_last and not before_last == "TRADE":
-                qr.pop()
-                popped_last = True
-                popped_last_was_set = True
-            qr.append(recent_bbo.copy())
-            if not popped_last_was_set:
-                popped_last = False
-        last = "ASK"
-    before_last = tmp
-
-for quote in qr:
-    print(quote)
-        
 session.stop()
