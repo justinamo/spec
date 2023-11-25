e2cf95de685b4429a22974593656e20de01e6179
Justin Moore <justinamo@gmail.com>
2023-11-25 15:43:17 -0500

    only working one way or the other prospectively

---
+++ dates.py
@@ -0,0 +1,17 @@
+import pytz
+from datetime import datetime
+    
+date_format = '%Y-%m-%dT%H:%M:%S'
+eastern = pytz.timezone("US/Eastern")
+utc = pytz.utc
+
+def convert_to_utc(year, month, day, hour, minute, second):
+    naive = datetime(year, month, day, hour, minute, second)
+    eastern_time = eastern.localize(naive)
+    utc_time = eastern_time.astimezone(utc)
+    return utc_time.strftime(date_format)
+
+def convert_from_utc(dt):
+    utc_time = utc.localize(dt)
+    eastern_time = utc_time.astimezone(eastern)
+    return eastern_time
